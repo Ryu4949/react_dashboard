1ed9ae7f86886bbb1e8e600266a6e25f6f92c029
Ryu4949 <kyong494949@gmail.com>
2023-10-30 16:00:00 +0900

    [Chore] install hookform error-message

--- src/partials/signin/SignInCard.jsx
+++ src/partials/signin/SignInCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+import useAuth from '../../hooks/useAuth';
 
 function SignInCard() {
 
